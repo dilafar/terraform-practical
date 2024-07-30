27601b6dc11a3ee2f34f946ec9c6ca1b1bf30fd3
dilafar <it20784720@my.sliit.lk>
2024-07-31 01:48:51 +0530

    ec2 instance ami keypair added

--- main.tf
+++ main.tf
@@ -7,6 +7,9 @@ variable "subnet_cidr_block" {}
 variable "availability_zone" {}
 variable "env_prefix" {}
 variable "my_ip" {}
+variable "private_key_location" {}
+variable "public_key_location" {}
+variable "instance_type" {}
 
 resource "aws_vpc" "dev-vpc" {
   cidr_block           = var.vpc_cidr_block
@@ -119,9 +122,50 @@ resource "aws_default_security_group" "dev-sg" {
 }
 
 */
+data "aws_ami" "dev-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
+  tags = {
+    Name = "${var.env_prefix}-ami"
+  }
 
-output "dev-vpc-id" {
-  value = aws_vpc.dev-vpc.id
 }
+
+resource "aws_key_pair" "dev-key" {
+  key_name   = "server-key"
+  public_key = file(var.public_key_location)
+}
+
+resource "aws_instance" "dev-instance" {
+  ami                         = data.aws_ami.dev-ami.id
+  instance_type               = var.instance_type
+  availability_zone           = var.availability_zone
+  subnet_id                   = aws_subnet.dev-subnet.id
+  vpc_security_group_ids      = [aws_security_group.dev-sg.id]
+  key_name                    = aws_key_pair.dev-key.key_name
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "${var.env_prefix}-instance"
+  }
+
+}
+
+output "ami-id" {
+  value = data.aws_ami.dev-ami
+}
+
+output "public-ip" {
+  value = aws_instance.dev-instance.public_ip
+}
+
 
