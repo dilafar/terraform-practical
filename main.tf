7149450dbe4a9d433fca6447b370d21b8b181f92
dilafar <it20784720@my.sliit.lk>
2024-07-30 23:35:17 +0530

    route table added with subnet association

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "availability_zone" {}
+variable "env_prefix" {}
 
 resource "aws_vpc" "dev-vpc" {
   cidr_block           = var.vpc_cidr_block
@@ -12,7 +13,7 @@ resource "aws_vpc" "dev-vpc" {
   enable_dns_support   = true
 
   tags = {
-    Name : "vpc_dev"
+    Name = "${var.env_prefix}-vpc"
   }
 
 }
@@ -23,9 +24,34 @@ resource "aws_subnet" "dev-subnet" {
   cidr_block        = var.subnet_cidr_block
 
   tags = {
-    Name : "subnet_dev"
+    Name = "${var.env_prefix}-subnet"
   }
 
+}
+
+resource "aws_internet_gateway" "dev-gateway" {
+  vpc_id = aws_vpc.dev-vpc.id
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "dev-route" {
+  vpc_id = aws_vpc.dev-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev-gateway.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-route_table"
+  }
+}
+
+resource "aws_route_table_association" "route-association-dev" {
+  route_table_id = aws_route_table.dev-route.id
+  subnet_id      = aws_subnet.dev-subnet.id
 }
 
 output "dev-vpc-id" {
